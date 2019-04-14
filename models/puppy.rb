2cf3b5104352b84e745ee912780312e1002bddde
Dan Hintz <dhintz@live.com>
2019-04-14 17:15:43 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,8 +1,10 @@
 class Puppy < Sinatra
   attr_reader :name, :breed, :months_old
   
-  def initialize(name, breed, months_old)
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @months_old = age
+  end
+  
 end
