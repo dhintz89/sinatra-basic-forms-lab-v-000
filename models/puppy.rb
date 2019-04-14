b2d62cd5c740b90d5477de9edeae4b4b5c2f53ed
Dan Hintz <dhintz@live.com>
2019-04-14 17:46:06 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,10 @@
 class Puppy
   attr_accessor :name, :breed, :age
   
-  def initialize()
-    @name = params[:name]
-    @breed = params[:breed]
-    @age = params[:age]
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
   end
   
 end
